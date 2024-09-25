91c34d4dca9ce304f5afb4e6093cd6fe54a09c74
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-09-25 14:57:00 +0530

    feat (UI) : create candidata home page

---
+++ frontend/AIPT/src/pages/CandidateHome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CandidateHome: React.FC = () => {
+  return (
+    <div>
+      <h2>Welcome, Candidate!</h2>
+      <p>This is the home page for candidates.</p>
+    </div>
+  );
+};
+
+export default CandidateHome;
