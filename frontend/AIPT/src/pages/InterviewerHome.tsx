5f1311a9dd6e949a6879c723fbb3d9bf8a512c9e
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-09-25 14:56:36 +0530

    feat(UI) : create interviewer home page

---
+++ frontend/AIPT/src/pages/InterviewerHome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InterviewerHome: React.FC = () => {
+  return (
+    <div>
+      <h2>Welcome, Interviewer!</h2>
+      <p>This is the home page for interviewers.</p>
+    </div>
+  );
+};
+
+export default InterviewerHome;
