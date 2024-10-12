ec3e6235e50839bdd8227844ec0340115bb25980
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-12 14:15:49 +0530

    fix (config) : fixed routing problems

--- frontend/AIPT/src/App.tsx
+++ frontend/AIPT/src/App.tsx
@@ -2,45 +2,101 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
-import InterviewerHome from './pages/InterviewerHome';
-import CandidateHome from './pages/CandidateHome';
+import InterviewerHome from './pages/Interviewer/InterviewerHome';
+import CandidateHome from './pages/Candidate/CandidateHome';
+import PrivateRoute from './components/PrivateRoute';
+import Home from './pages/Home';
+import Profile from './pages/Interviewer/Profile';
+import Interviews from './pages/Interviewer/Interviews';
+import Assignments from './pages/Candidate/Assignments';
+import CandidateProfile from './pages/Candidate/CandidateProfile';
+import Settings from './pages/Candidate/Settings';
 
 const App: React.FC = () => {
-  const token = localStorage.getItem('token');
-  const userRole = localStorage.getItem('role');
-
-  const PrivateRoute = ({ children, role }: { children: JSX.Element, role: string }) => {
-    if (!token) {
-      return <Navigate to="/login" />;
-    }
-
-    if (role && userRole !== role) {
-      return <Navigate to="/" />;
-    }
-
-    return children;
-  };
 
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route 
-        path="/interviewer-home" 
+      <Route path="/" element={<Home />} />
+
+      {/* Routes for Interviewer */}
+
+      <Route
+        path="/interviewer-home"
         element={
-          <PrivateRoute role="interviewer">
+          <PrivateRoute allowedRoles={['interviewer']}>
             <InterviewerHome />
           </PrivateRoute>
-        } 
+        }
       />
-      <Route 
-        path="/candidate-home" 
+      <Route
+        path="/interviewer-profile"
         element={
-          <PrivateRoute role="candidate">
+          <PrivateRoute allowedRoles={['interviewer']}>
+            <Profile />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/interviewer-interviews"
+        element={
+          <PrivateRoute allowedRoles={['interviewer']}>
+            <Interviews />
+          </PrivateRoute>
+        }
+      />
+
+
+      {/* Routes for Candidates */}
+
+      <Route
+        path="/candidate-home"
+        element={
+          <PrivateRoute allowedRoles={['candidate']}>
             <CandidateHome />
           </PrivateRoute>
-        } 
+        }
       />
+      <Route
+        path="/assignments"
+        element={
+          <PrivateRoute allowedRoles={['candidate']}>
+            <Assignments />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/candidate-home"
+        element={
+          <PrivateRoute allowedRoles={['candidate']}>
+            <CandidateHome />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute allowedRoles={['candidate']}>
+            <CandidateProfile />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/settings"
+        element={
+          <PrivateRoute allowedRoles={['candidate']}>
+           <Settings />
+          </PrivateRoute>
+        }
+      />
+
+
+
+
+
+
     </Routes>
   );
 }
